fd47b6b5aa9d25ac6588a37353263b8c0a4506a7
wasuken <wevorence@gmail.com>
2023-04-28 20:25:00 +0900

    画像アップローダにバルーンを追加

--- src/components/GroupImageUploader.tsx
+++ src/components/GroupImageUploader.tsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import styles from "@/styles/GroupImageUploader.module.css";
 
 type Props = {
-  onUpload: (file: File) => void;
+  onUpload: (file: File, displayNo: number) => Promise<Response>;
 };
 
 const GroupImageUploader: React.FC<Props> = ({ onUpload }) => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
   const [displayNo, setDisplayNo] = useState<number>(0);
+  const [isBalloon, setIsBalloon] = useState(false);
+  const [balloonMessage, setBalloonMessage] = useState("");
 
   const handleFileInputChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -26,20 +28,40 @@ const GroupImageUploader: React.FC<Props> = ({ onUpload }) => {
     }
   };
 
-  const handleUploadClick = () => {
+  const handleUploadClick = async () => {
     if (selectedFile) {
-      onUpload(selectedFile);
-      setSelectedFile(null);
-      setPreviewUrl(null);
+      const res = await onUpload(selectedFile, displayNo);
+      setIsBalloon(true);
+      if (res.ok) {
+        setSelectedFile(null);
+        setPreviewUrl(null);
+        setBalloonMessage("アップロードに成功しました。");
+      } else {
+        setBalloonMessage("アップロードに失敗しました。");
+      }
     }
   };
   const handleDeleteClick = () => {
     setSelectedFile(null);
     setPreviewUrl(null);
   };
+  const handleBalloonDeleteClick = () => {
+    setIsBalloon(false);
+  };
 
   return (
     <div className={styles.container}>
+      {isBalloon && (
+        <div className={styles.balloonContainer}>
+          <div className={styles.message}>{balloonMessage}</div>
+          <button
+            onClick={handleBalloonDeleteClick}
+            className={styles.balloonDeleteButton}
+          >
+            &#x2716;
+          </button>
+        </div>
+      )}
       <h2>Group Image Uploader</h2>
       {previewUrl && (
         <div className={styles.previewContainer}>
