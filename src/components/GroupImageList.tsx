c73e0f5f381b0b899ee59736ceea85b9e5bf6f58
wasuken <wevorence@gmail.com>
2023-05-03 11:17:23 +0900

    画像周りを修正。表示番号も表示。

--- src/components/GroupImageList.tsx
+++ src/components/GroupImageList.tsx
@@ -1,7 +1,9 @@
 import styles from "@/styles/GroupImageList.module.css";
+import Image from "next/image";
+import { Image as IImage } from "@/types";
 
 type Props = {
-  images: string[];
+  images: IImage[];
 };
 
 const GroupImageList: React.FC<Props> = ({ images }) => {
@@ -9,12 +11,15 @@ const GroupImageList: React.FC<Props> = ({ images }) => {
     <div className={styles.container}>
       <h2>Group Image List</h2>
       <div className={styles.list}>
-        {(images ?? []).map((url, index) => (
+        {(images ?? []).map((image, index) => (
           <div key={index} className={styles.item}>
-            <img
-              src={url}
+            <span className={styles.displayNo}>{image.displayNo}</span>
+            <Image
+              src={`http://localhost:3000${image.path}`}
               alt={`Group Image ${index + 1}`}
               className={styles.image}
+              width={300}
+              height={500}
             />
           </div>
         ))}
