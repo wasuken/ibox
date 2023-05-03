18eb082e9e3fcca1932043ded97ab4b5dca45afc
wasuken <wevorence@gmail.com>
2023-05-03 11:19:41 +0900

    更新後データ再取得するように修正

--- src/pages/group/[id].tsx
+++ src/pages/group/[id].tsx
@@ -4,8 +4,7 @@ import { Group } from "@/types";
 import { PrismaClient } from "@prisma/client";
 
 import GroupInfo from "@/components/GroupInfo";
-import GroupImageList from "@/components/GroupImageList";
-import GroupImageUploader from "@/components/GroupImageUploader";
+import GroupImageListUp from "@/components/GroupImageListUp";
 import styles from "@/styles/GroupDetail.module.css";
 
 import { useState } from "react";
@@ -16,16 +15,18 @@ type Props = {
   group: Group;
 };
 
+const emptyResponse = {
+  props: {
+    group: {},
+  },
+};
+
 export const getServerSideProps: GetServerSideProps<Props> = async (
   context
 ) => {
   const { id } = context.query;
   if (typeof id !== "string") {
-    return {
-      props: {
-        group: {},
-      },
-    };
+    return emptyResponse;
   }
   const group = await prisma.group.findFirst({
     where: {
@@ -49,17 +50,29 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
           image: {
             select: {
               path: true,
+              name: true,
+              size: true,
             },
           },
+          display_no: true,
+        },
+        orderBy: {
+          display_no: "asc",
         },
       },
     },
   });
+  if (!group) return emptyResponse;
   const jgroup = {
     ...JSON.parse(JSON.stringify(group)),
     name: group.title,
     tags: group.groupTags.map((t) => t.tag.name),
-    images: group?.groupImages,
+    images: group?.groupImages.map((gi) => {
+      return {
+        ...gi.image,
+        displayNo: gi.display_no,
+      };
+    }),
   };
   return {
     props: {
@@ -70,35 +83,50 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
 
 const GroupPage: NextPage<Props> = (props: Props) => {
   const [selectedItem, setSelectedItem] = useState(0);
-  const postImage = (imageFile: File, displayNo: number) => {
+  const [group, setGroup] = useState<Group>(props.group);
+  const fetchGroup = async () => {
+    const id = props.group.id;
+    try {
+      const res = await fetch(`/api/group/${id}`);
+      const resj = await res.json();
+      setGroup(resj);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+  const postImage = async (
+    imageFile: File,
+    displayNo: number,
+    fileName: string
+  ) => {
     const formData = new FormData();
     formData.set("image", imageFile);
-    formData.set("name", imageFile.name);
+    formData.set("name", fileName);
     formData.set("size", imageFile.size.toString());
     formData.set("displayNo", displayNo.toString());
-    return fetch(`/api/group/image`, {
+    formData.set("groupId", group.id.toString());
+    const res = await fetch(`/api/group/image`, {
       method: "POST",
       body: formData,
     });
+    const ress = await fetchGroup();
+    return [res.ok, ress];
   };
-  const updateGroup = (group: Group) => {
-    fetch(`/api/group`, {
+  const updateGroup = async (group: Group) => {
+    const res = await fetch(`/api/group/${group.id}`, {
       method: "PUT",
       body: JSON.stringify(group),
       headers: {
         "Content-Type": "application/json",
       },
     });
+    return res.ok;
   };
-  const menuItems = [
-    "グループ詳細",
-    "グループ画像アップロード",
-    "グループ画像リスト",
-  ];
+  const menuItems = ["グループ詳細", "アップロード/画像リスト"];
   const contents = [
-    <GroupInfo key={0} group={props.group} onSave={updateGroup} />,
-    <GroupImageUploader key={1} onUpload={postImage} />,
-    <GroupImageList key={2} images={props.group.images} />,
+    <GroupInfo key={0} group={group} onSave={updateGroup} />,
+    <GroupImageListUp key={1} onUpload={postImage} images={group.images} />,
   ];
   return (
     <Layout>
