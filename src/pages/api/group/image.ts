e9a3e49eab3cef1976705f81e02e03b2922d3f9d
wasuken <wevorence@gmail.com>
2023-05-04 17:32:23 +0900

    画像保存処理修正。重複とかの対策

--- src/pages/api/group/image.ts
+++ src/pages/api/group/image.ts
@@ -3,6 +3,7 @@ import formidable from "formidable";
 import { PrismaClient } from "@prisma/client";
 import fs from "fs";
 import path from "path";
+import { v4 as uuidv4 } from "uuid";
 
 const prisma = new PrismaClient();
 
@@ -19,7 +20,6 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     const form = new formidable.IncomingForm();
-
     try {
       form.parse(req, async function (err, fields, files) {
         if (err) {
@@ -30,11 +30,24 @@ export default async function handler(
           return;
         }
         const file = files.image as formidable.File;
+        const size = file.size;
+        const displayNo = fields.displayNo as string;
+        const groupId = fields.groupId as string;
+
         const originPath = file.filepath;
         const newDir = "./public/uploads";
         const name = fields.name as string;
-        const newPath = path.join(newDir, name);
-        const webPath = path.join("/uploads", name);
+        const fname = `${uuidv4()}_${name}`;
+        // グループIDごとに画像を作成する。
+        const newPath = path.join(newDir, groupId, fname);
+        const webPath = path.join("/uploads", groupId, fname);
+
+        const dirPath = newPath.replace(/\/[^/]*$/, "");
+        try {
+          fs.mkdirSync(dirPath, { recursive: true });
+        } catch (err) {
+          console.log("exists dir");
+        }
 
         fs.copyFile(originPath, newPath, (err) => {
           if (err) throw err;
@@ -43,13 +56,10 @@ export default async function handler(
           });
         });
 
-        const size = file.size;
-        const displayNo = fields.displayNo as string;
-        const groupId = fields.groupId as string;
         await prisma.image.create({
           data: {
             path: webPath,
-            name,
+            name: fname,
             size,
             groupImages: {
               create: [
