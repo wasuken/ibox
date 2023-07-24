86aef7aa583c1b9b8b68c04f847a10cb5c55a2d8
wasuken <wevorence@gmail.com>
2023-07-24 14:02:42 +0900

    グループリスト全体ではなくタイトルで遷移するように、またタグによる遷移も可能に

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -13,11 +13,7 @@ export default function GroupList(props: Props) {
       <h2>グループ</h2>
       <div className={styles["group-list"]}>
         {groupList.map((group, i) => (
-          <a
-            href={`/group/${group.id}`}
-            className={styles["group-item"]}
-            key={i}
-          >
+          <div key={i} className={styles["group-item"]}>
             <div>
               {group.images?.length > 0 && (
                 <NextImage
@@ -27,15 +23,21 @@ export default function GroupList(props: Props) {
                   alt={group.images[0].name}
                 />
               )}
-              <h3>{group.name}</h3>
+              <h3>
+                <a href={`/group/${group.id}`}>{group.name}</a>
+              </h3>
               <p>
                 {group.tags &&
                   group.tags.length >= 0 &&
-                  group.tags.map((t) => t.name).join(", ")}
+                  group.tags.map((t, i) => (
+                    <a key={i} href={`/?tag=${t.name}&page=0&query=`}>
+                      {t.name}
+                    </a>
+                  ))}
               </p>
               <p>{group.createdAt.toLocaleString()}</p>
             </div>
-          </a>
+          </div>
         ))}
       </div>
     </div>
